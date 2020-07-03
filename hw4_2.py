4bdf98ab00477f718aa8f7d6ec7dfe51921e06a5
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2020-07-03 20:54:12 +0530

    Create hw4_2.py

---
+++ hw4_2.py
@@ -0,0 +1,5 @@
+hour = int(input("enter number of hours: "))
+minute = int(input("enter number of minutes: "))
+sec = int(input("enter number of seconds: "))
+time_sec = ((hour*3600)+(minute*60)+(sec))
+print (" time in seconds is: ",time_sec)
