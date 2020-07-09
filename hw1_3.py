7f027bf1a8345a18717a269fa0d8291915ed75af
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2020-07-09 15:37:01 +0530

    Update hw1_3.py

--- hw1_3.py
+++ hw1_3.py
@@ -1,4 +1,9 @@
-mon,tue,wed = 24,17,21
-thu,fri,sat,sun = 12,28,27,24
+mon = float(input("enter temperature: "))
+tue = float(input("enter temperature: "))
+wed = float(input("enter temperature: "))
+thu = float(input("enter temperature: "))
+fri = float(input("enter temperature: "))
+sat = float(input("enter temperature: "))
+sun = float(input("enter temperature: "))
 avg = (mon+tue+wed+thu+fri+sat+sun)/7
 print (avg)
