a6eaed22dec5f34388c0547c6f73e3e2c6863007
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2020-07-24 12:05:20 +0530

    Create if_else_5.py

---
+++ if_else_5.py
@@ -0,0 +1,6 @@
+num_1 = int(input("enter a number: "))
+num_2 = int(input("enter another number: "))
+if num_1%7 == 0 and num_2%7 == 0:
+    print ("both numbers are multiples of 7 ")
+else:
+    print ("both numbers are not multiples of 7")
