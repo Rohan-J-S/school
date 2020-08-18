ec71f4b0dfb4be0db6cae09a9b66cd725c327a5e
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2020-08-18 09:39:18 +0530

    Update amstrong.py

--- amstrong.py
+++ amstrong.py
@@ -1,11 +1,11 @@
 num = int(input("enter a number: "))
 str_num = str(num)
-digits = len(str_num)
-var_1 = num
+
 var_2 = 0
 for x in (str_num):
     var_2 += int(x)**len(str_num)
-if var_1 == var_2:
+if num == var_2:
     print("it is an amstrong number ")
 else:
     print("it is not an amstrong number")
+
