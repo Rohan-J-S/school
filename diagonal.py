f89f63f05218c0611df0c855635dd2e113d670ab
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2020-11-06 10:21:56 +0530

    Create diagonal.py

---
+++ diagonal.py
@@ -0,0 +1,31 @@
+r = int(input(",,"))
+c = int(input(".."))
+grid = []
+s = 0
+for x in range(r):
+    col = []
+    for y in range(c):
+        col += [0]
+    grid += [col]
+print (grid)
+left = 0
+right = 1
+for i in range(r):
+    for j in range(c):
+        print ("row: ",i+1,"col: ",j+1)
+        grid[i][j] = int(input())
+        if j == i:
+            left += grid[i][j]
+            
+        elif i+j == r-1:
+            right *= grid[i][j]
+        
+for x in range(r):
+    for y in range(c):
+        if x == y:
+            print (grid[x][y],end = '')
+        elif x+y == r-1:
+            print(grid[x][y],end = "")
+        else:
+            print(" ",end = "")
+    print ()
