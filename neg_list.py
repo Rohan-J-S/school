9dbb49d5445f8fc9114553497dafffc8bdd0f6aa
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2020-11-17 13:02:21 +0530

    Update neg_list.py

--- neg_list.py
+++ neg_list.py
@@ -3,11 +3,13 @@ l = []
 for x in range(days):
     item = float(input("enter temp "))
     l += [item]
+j = 0
 for y in range(len(l)):
     if l[y] < 0:
-        z = l[0]
-        l[0] = l[y]
-        for i in range(y,1,-1):
-            
+        z = l[j]
+        l[j] = l[y]
+        for i in range(y,j+1,-1):          
             l[i] = l[i-1]
-        l[1] = z
+        j += 1
+        l[j+1] = z
+print (l)
